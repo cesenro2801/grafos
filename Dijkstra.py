2ab68414dc95847665b1b3dcc8d4367812a0426a
Cesar_rojas <cesar2191952@correo.uis.edu.co>
2022-03-12 19:00:54 -0500

    TERMINADO

--- Dijkstra.py
+++ Dijkstra.py
@@ -42,451 +42,452 @@ graph = {
         'kiosco residencias': 25,
         'N28': 120,
         'N19': 42,
-        'coliseo': 1
+        'coliseo': 53
     },
     'coliseo': {
-        'porteria carrera 30': 1,
-        'N21': 1,
-        'N20': 1,
-        'cancha de futbol sur': 1,
-        'N29': 1
+        'porteria carrera 30': 69,
+        'N21': 44,
+        'N20': 53,
+        'cancha de futbol sur': 60,
+        'N29': 89
     },
     'N28': {
-        'N20': 1,
-        'N19': 1,
-        'N27': 1,
-        'canchas multiples': 1
+        'N20': 120,
+        'N19': 150,
+        'N27': 63,
+        'canchas multiples': 52
     },
     'canchas multiples': {
-        'N28': 1,
-        'N27': 1
+        'N28': 52,
+        'N27': 35
     },
     'N27': {
-        'canchas multiples': 1,
-        'N26': 1
+        'canchas multiples': 35,
+        'N28': 63,
+        'N26': 81
     },
     'N19': {
-        'N20': 1,
-        'N28': 1,
-        'N18': 1,
-        'cancha de futbol sur': 1
+        'N20': 42,
+        'N28': 150,
+        'N18': 74,
+        'cancha de futbol sur': 38
     },
     'cancha de futbol sur': {
-        'coliseo': 1,
-        'N19': 1,
-        'N29': 1
+        'coliseo': 60,
+        'N19': 38,
+        'N29': 35
     },
     'N29': {
-        'porteria carrera 30': 1,
-        'coliseo': 1,
-        'cancha de futbol sur': 1,
-        'N30': 1
+        'porteria carrera 30': 120,
+        'coliseo': 89,
+        'cancha de futbol sur': 35,
+        'N30': 135
     },
     'N26': {
-        'N27': 1,
-        'N25': 1
+        'N27': 81,
+        'N25': 73
     },
     'N25': {
-        'N26': 1,
-        'N24': 1,
-        'administracion 2': 1,
-        'bienestar universitario': 1
+        'N26': 73,
+        'N24': 68,
+        'administracion 2': 30,
+        'bienestar universitario': 54
     },
     'N24': {
-        'cancha 1 marzo': 1,
-        'N25': 1,
-        'insed': 1,
-        'N33': 1,
-        'administracion': 1,
-        'N17': 1
+        'cancha 1 marzo': 74,
+        'N25': 68,
+        'insed': 43,
+        'N33': 54,
+        'administracion': 46,
+        'N17': 79
     },
     'cancha 1 marzo': {
-        'N24': 1
+        'N24': 74
     },
     'N18': {
-        'N19': 1,
-        'canchas de tenis': 1,
-        'N17': 1
+        'N19': 74,
+        'canchas de tenis': 45,
+        'N17': 55
     },
     'canchas de tenis': {
-        'N18': 1
+        'N18': 45
     },
     'N17': {
-        'N18': 1,
-        'N24': 1,
-        'N31': 1,
-        'administracion': 1
+        'N18': 55,
+        'N24': 79,
+        'N31': 50,
+        'administracion': 68
     },
     'N31': {
-        'N17': 1,
-        'N30': 1
+        'N17': 50,
+        'N30': 70
     },
     'N30': {
-        'N31': 1,
-        'N29': 1,
-        'N32': 1
+        'N31': 70,
+        'N29': 135,
+        'N32': 58
     },
     'N32': {
-        'N30': 1,
-        'auditorio luis a. calvo': 1,
-        'porteria carrer 27': 1
+        'N30': 58,
+        'auditorio luis a. calvo': 43,
+        'porteria carrer 27': 39
     },
     'porteria carrer 27': {
-        'N32': 1,
-        'P4': 1
+        'N32': 39,
+        'P4': 20
     },
     'auditorio luis a. calvo': {
-        'N32': 1,
-        'P4': 1,
-        'N15': 1
+        'N32': 43,
+        'P4': 85,
+        'N15': 20
     },
     'N15': {
-        'auditorio luis a. calvo': 1,
-        'mantenimiento y planta fisica': 1,
-        'N14': 1
+        'auditorio luis a. calvo': 20,
+        'mantenimiento y planta fisica': 62,
+        'N14': 79
     },
     'N14': {
-        'administracion': 1,
-        'N34': 1,
-        'P3': 1,
-        'N15': 1
+        'administracion': 61,
+        'N34': 73,
+        'P3': 64,
+        'N15': 79
     },
     'administracion': {
-        'N17': 1,
-        'N24': 1,
-        'N33': 1,
-        'N14': 1
+        'N17': 68,
+        'N24': 46,
+        'N33': 25,
+        'N14': 61
     },
     'N33': {
-        'administracion': 1,
-        'N24': 1,
-        'insed': 1,
-        'N34': 1,
+        'administracion': 25,
+        'N24': 54,
+        'insed': 20,
+        'N34': 48
     },
     'insed': {
-        'N24': 1,
-        'administracion 2': 1,
-        'N34': 1,
-        'N33': 1
+        'N24': 43,
+        'administracion 2': 35,
+        'N34': 59,
+        'N33': 20
     },
     'administracion 2': {
-        'N25': 1,
-        'bienestar universitario': 1,
-        'N12': 1,
-        'N34': 1,
-        'insed': 1,
+        'N25': 30,
+        'bienestar universitario': 63,
+        'N12': 110,
+        'N34': 130,
+        'insed': 35,
     },
     'bienestar universitario': {
-        'N25': 1,
-        'administracion 2': 1,
-        'la perla': 1
+        'N25': 54,
+        'administracion 2': 63,
+        'la perla': 59
     },
     'la perla': {
-        'bienestar universitario': 1,
-        'N12': 1
+        'bienestar universitario': 59,
+        'N12': 41
     },
     'N12': {
-        'la perla': 1,
-        'N34': 1,
-        'ingenieria industrial': 1,
-        'N11': 1,
-        'administracion 2': 1
+        'la perla': 41,
+        'N34': 76,
+        'ingenieria industrial': 37,
+        'N11': 29,
+        'administracion 2': 110
     },
     'N34': {
-        'N33': 1,
-        'insed': 1,
-        'administracion 2': 1,
-        'N12': 1,
-        'instituto de lenguas': 1,
-        'N35': 1,
-        'N14': 1
+        'N33': 48,
+        'insed': 59,
+        'administracion 2': 130,
+        'N12': 76,
+        'instituto de lenguas': 82,
+        'N35': 78,
+        'N14': 73
     },
     'P4': {
-        'porteria carrer 27': 1,
-        'auditorio luis a. calvo': 1,
-        'mantenimiento y planta fisica': 1,
-        'N16': 1
+        'porteria carrer 27': 20,
+        'auditorio luis a. calvo': 85,
+        'mantenimiento y planta fisica': 51,
+        'N16': 64
     },
     'mantenimiento y planta fisica': {
-        'P4': 1,
-        'N15': 1,
-        'ingenieria mecanica': 1,
-        'N16': 1
+        'P4': 51,
+        'N15': 62,
+        'ingenieria mecanica': 31,
+        'N16': 39
     },
     'ingenieria mecanica': {
-        'mantenimiento y planta fisica': 1,
-        'P3': 1,
-        'aula max. de mecanica': 1,
-        'N10': 1
+        'mantenimiento y planta fisica': 31,
+        'P3': 62,
+        'aula max. de mecanica': 39,
+        'N10': 49
     },
     'P3': {
-        'ingenieria mecanica': 1,
-        'N14': 1,
-        'N35': 1,
-        'biblioteca': 1,
-        'N9': 1,
-        'aula max. de mecanica': 1
+        'ingenieria mecanica': 62,
+        'N14': 64,
+        'N35': 80,
+        'biblioteca': 60,
+        'N9': 38,
+        'aula max. de mecanica': 26
     },
     'N35': {
-        'P3': 1,
-        'N34': 1,
-        'cafeteria': 1,
-        'biblioteca': 1,
-        'instituto de lenguas': 1
+        'P3': 80,
+        'N34': 78,
+        'cafeteria': 30,
+        'biblioteca': 20,
+        'instituto de lenguas': 38
     },
     'instituto de lenguas': {
-        'N35': 1,
-        'N34': 1,
-        'N11': 1,
-        'cafeteria': 1,
+        'N35': 38,
+        'N34': 82,
+        'N11': 21,
+        'cafeteria': 23
     },
     'N11': {
-        'instituto de lenguas': 1,
-        'N12': 1,
-        'ingenieria industrial': 1,
-        'diseño industrial': 1
+        'instituto de lenguas': 21,
+        'N12': 37,
+        'ingenieria industrial': 27,
+        'diseño industrial': 60
     },
     'ingenieria industrial': {
-        'N12': 1,
-        'N11': 1
+        'N12': 37,
+        'N11': 27
     },
     'cafeteria': {
-        'N35': 1,
-        'instituto de lenguas': 1,
-        'planta telefonica': 1
+        'N35': 30,
+        'instituto de lenguas': 23,
+        'planta telefonica': 8
     },
     'biblioteca': {
-        'N35': 1,
-        'P3': 1,
-        'N36': 1
+        'N35': 20,
+        'P3': 60,
+        'N36': 18
     },
     'planta telefonica': {
-        'cafeteria': 1,
-        'N36': 1,
-        'diseño industrial': 1
+        'cafeteria': 8,
+        'N36': 22,
+        'diseño industrial': 26
     },
     'aula max. de mecanica': {
-        'ingenieria mecanica': 1,
-        'P3': 1,
-        'N9': 1,
-        'N10': 1
+        'ingenieria mecanica': 39,
+        'P3': 26,
+        'N9': 15,
+        'N10': 37
     },
     'ciencias humanas': {
-        'N16': 1
+        'N16': 28
     },
     'N16': {
-        'ciencias humanas': 1,
-        'P4': 1,
-        'mantenimiento y planta fisica': 1,
-        'N10': 1,
-        'N39': 1
+        'ciencias humanas': 28,
+        'P4': 64,
+        'mantenimiento y planta fisica': 39,
+        'N10': 72,
+        'N39': 94
     },
     'N10': {
-        'N16': 1,
-        'ingenieria mecanica': 1,
-        'aula max. de mecanica': 1,
-        'N9': 1,
-        'camilo torres': 1,
-        'laboratorios livianos': 1,
-        'N39': 1
+        'N16': 72,
+        'ingenieria mecanica': 49,
+        'aula max. de mecanica': 37,
+        'N9': 56,
+        'camilo torres': 44,
+        'laboratorios livianos': 55,
+        'N39': 87
     },
     'N9': {
-        'aula max. de mecanica': 1,
-        'P3': 1,
-        'N36': 1,
-        'N8': 1,
-        'camilo torres': 1,
-        'N10': 1
+        'aula max. de mecanica': 15,
+        'P3': 38,
+        'N36': 53,
+        'N8': 40,
+        'camilo torres': 19,
+        'N10': 56
     },
     'N36': {
-        'N9': 1,
-        'biblioteca': 1,
-        'planta telefonica': 1,
-        'capruis y favuis': 1,
-        'N37': 1
+        'N9': 53,
+        'biblioteca': 12,
+        'planta telefonica': 22,
+        'capruis y favuis': 40,
+        'N37': 65
     },
     'diseño industrial': {
-        'planta telefonica': 1,
-        'N11': 1,
-        'ingenieria electrica': 1,
-        'N7': 1,
-        'capruis y favuis': 1
+        'planta telefonica': 26,
+        'N11': 60,
+        'ingenieria electrica': 42,
+        'N7': 16,
+        'capruis y favuis': 27
     },
     'N7': {
-        'diseño industrial': 1,
-        'ingenieria electrica': 1,
-        'lab hidraulica': 1
+        'diseño industrial': 16,
+        'ingenieria electrica': 129,
+        'lab hidraulica': 21
     },
     'ingenieria electrica': {
-        'N7': 1,
-        'ingenieria electrica': 1,
-        'P1': 1
+        'N7': 29,
+        'diseño industrial': 42,
+        'P1': 30
     },
     'invernadero': {
-        'P1': 1,
-        'caracterizacion de mat.': 1
+        'P1': 56,
+        'caracterizacion de mat.': 67
     },
     'P1': {
-        'ingenieria electrica': 1,
-        'invernadero': 1,
-        'caracterizacion de mat.': 1,
-        'torres diseño industrial': 1
+        'ingenieria electrica': 30,
+        'invernadero': 56,
+        'caracterizacion de mat.': 42,
+        'torres diseño industrial': 43
     },
     'caracterizacion de mat.': {
-        'invernadero': 1,
-        'P1': 1,
-        'torres diseño industrial': 1
+        'invernadero': 67,
+        'P1': 42,
+        'torres diseño industrial': 52
     },
     'N39': {
-        'N16': 1,
-        'N10': 1,
-        'N1': 1
+        'N16': 94,
+        'N10': 87,
+        'N1': 98
     },
     'laboratorios livianos': {
-        'N10': 1,
-        'N38': 1
+        'N10': 55,
+        'N38': 38
     },
     'camilo torres': {
-        'N10': 1,
-        'N9': 1,
-        'N8': 1
+        'N10': 44,
+        'N9': 19,
+        'N8': 15
     },
     'N8': {
-        'N9': 1,
-        'camilo torres': 1,
-        'centic': 1,
-        'N3': 1,
-        'N38': 1
+        'N9': 40,
+        'camilo torres': 15,
+        'centic': 35,
+        'N3': 40,
+        'N38': 32
     },
     'centic': {
-        'N8': 1,
-        'N3': 1
+        'N8': 35,
+        'N3': 52
     },
     'capruis y favuis': {
-        'N37': 1,
-        'N36': 1,
-        'diseño industrial': 1,
-        'aula maxima de fisica': 1
+        'N37': 15,
+        'N36': 40,
+        'diseño industrial': 27,
+        'aula maxima de fisica': 17
     },
     'lab hidraulica': {
-        'N7': 1,
-        'N6': 1,
-        'aula maxima de fisica': 1
+        'N7': 21,
+        'N6': 28,
+        'aula maxima de fisica': 51
     },
     'N38': {
-        'laboratorios livianos': 1,
-        'N8': 1,
-        'P2': 1,
-        'lab. de posgrados': 1
+        'laboratorios livianos': 38,
+        'N8': 32,
+        'P2': 32,
+        'lab. de posgrados': 40
     },
     'N3': {
-        'N8': 1,
-        'N37': 1,
-        'ingenieria quimica': 1,
-        'P2': 1
+        'N8': 35,
+        'N37': 19,
+        'ingenieria quimica': 15,
+        'P2': 37,
+        'centic': 52,
     },
     'N37': {
-        'N36': 1,
-        'aula maxima de fisica': 1,
-        'N3': 1
+        'N36': 65,
+        'aula maxima de fisica': 21,
+        'N3': 19
     },
     'aula maxima de fisica': {
-        'N37': 1,
-        'capruis y favuis': 1,
-        'lab hidraulica': 1,
-        'ceiam': 1
+        'N37': 21,
+        'capruis y favuis': 17,
+        'lab hidraulica': 51,
+        'ceiam': 23
     },
     'torres diseño industrial': {
-        'P1': 1,
-        'caracterizacion de mat.': 1,
-        'laboratorio alta tension': 1,
-        'N6': 1
+        'P1': 43,
+        'caracterizacion de mat.': 52,
+        'laboratorio alta tension': 27,
+        'N6': 22
     },
     'laboratorio alta tension': {
-        'torres diseño industrial': 1,
-        'N6': 1
+        'torres diseño industrial': 12,
+        'N6': 9
     },
     'N6': {
-        'N4': 1,
-        'lab hidraulica': 1,
-        'laboratorio alta tension': 1,
-        'torres diseño industrial': 1,
-        'N5': 1,
-        'ceiam': 1
+        'lab hidraulica': 28,
+        'laboratorio alta tension': 9,
+        'torres diseño industrial': 22,
+        'N5': 27,
+        'ceiam': 38
     },
     'ceiam': {
-        'N6': 1,
-        'N4': 1,
-        'aula maxima de fisica': 1
+        'N6': 38,
+        'N4': 12,
+        'aula maxima de fisica': 23
     },
     'N4': {
-        'ingenieria quimica': 1,
-        'ceiam': 1,
-        'facultad ing. fisicomecanicas': 1
+        'ingenieria quimica': 30,
+        'ceiam': 12,
+        'facultad ing. fisicomecanicas': 21
     },
     'ingenieria quimica': {
-        'N3': 1,
-        'N4': 1
+        'N3': 15,
+        'N4': 30
     },
     'P2': {
-        'N3': 1,
-        'N38': 1,
-        'lab. de posgrados': 1,
-        'N2': 1,
-        'jorge bautizta v': 1
+        'N3': 37,
+        'N38': 32,
+        'lab. de posgrados': 25,
+        'N2': 32,
+        'jorge bautizta v': 42
     },
     'lab. de posgrados': {
-        'N38': 1,
-        'P2': 1,
-        'N1': 1
+        'N38': 40,
+        'P2': 25,
+        'N1': 69
     },
     'N1': {
-        'N39': 1,
-        'lab. de posgrados': 1,
-        'N2': 1,
-        'planta de aceros': 1,
-        'jardineria': 1
+        'N39': 98,
+        'lab. de posgrados': 69,
+        'N2': 67,
+        'planta de aceros': 32,
+        'jardineria': 38
     },
     'N2': {
-        'N1': 1,
-        'P2': 1,
-        'jorge bautizta v': 1,
-        'planta de aceros': 1
+        'N1': 67,
+        'P2': 32,
+        'jorge bautizta v': 35,
+        'planta de aceros': 33
     },
     'jorge bautizta v': {
-        'P2': 1,
-        'N2': 1,
-        'porteria carrera 25': 1
+        'P2': 42,
+        'N2': 35,
+        'porteria carrera 25': 62
     },
     'planta de aceros': {
-        'N1': 1,
-        'N2': 1,
-        'porteria carrera 25': 1,
-        'jardineria': 1
+        'N1': 32,
+        'N2': 33,
+        'porteria carrera 25': 41,
+        'jardineria': 38
     },
     'jardineria': {
-        'N1': 1,
-        'planta de aceros': 1,
-        'porteria carrera 25': 1
+        'N1': 37,
+        'planta de aceros': 38,
+        'porteria carrera 25': 45
     },
     'porteria carrera 25': {
-        'jardineria': 1,
-        'planta de aceros': 1,
-        'jorge bautizta v': 1,
-        'facultad ing. fisicomecanicas': 1
+        'jardineria': 45,
+        'planta de aceros': 41,
+        'jorge bautizta v': 62,
+        'facultad ing. fisicomecanicas': 114
     },
     'facultad ing. fisicomecanicas': {
-        'porteria carrera 25': 1,
-        'N4': 1,
-        'daniel casas': 1
+        'porteria carrera 25': 114,
+        'N4': 21,
+        'daniel casas': 23
     },
     'daniel casas': {
-        'jorge bautizta v': 1,
-        'N5': 1
+        'jorge bautizta v': 23,
+        'N5': 14
     },
     'N5': {
-        'N6': 1,
-        'daniel casas': 1
+        'N6': 27,
+        'daniel casas': 14
     }
 }
 
@@ -543,7 +544,7 @@ def dijkstra(graph, start, goal):
         print('')
 
 
-dijkstra(graph, 'canchas de tenis', 'facultad ing. fisicomecanicas')
+dijkstra(graph, 'canchas de tenis', 'P1')
 dijkstra(graph, 'porteria carrera 25', 'P2')
 dijkstra(graph, 'porteria carrera 25', 'P3')
 dijkstra(graph, 'porteria carrera 25', 'P4')
